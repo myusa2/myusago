66311608124e604cfa937a45dcd99eb8802a7a9b
myusa2 <myusa.95.222@gmail.com>
2020-06-14 17:53:34 +0900

    足し算と引き算を追加

--- main.go
+++ main.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"unicode"
 )
+
+func at(s string, i int) rune {
+	return []rune(s)[i]
+}
+
+func strtoi(input string, pos int) (int, int) {
+	var res = 0
+	for pos < len(input) && unicode.IsDigit(at(input, pos)) {
+		res = res*10 + int(at(input, pos)-'0')
+		pos++
+	}
+	return res, pos
+}
 
 func run() int {
 	if len(os.Args) != 2 {
@@ -15,12 +28,30 @@ func run() int {
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global main\n")
 	fmt.Printf("main:\n")
-	var n, err = strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "引数が整数ではありません。\n")
+
+	var input = os.Args[1]
+	num, pos := strtoi(input, 0)
+
+	fmt.Printf("  mov rax, %d\n", num)
+	for pos < len(input) {
+		if at(input, pos) == '+' {
+			pos++
+			num, pos = strtoi(input, pos)
+			fmt.Printf("  add rax, %d\n", num)
+			continue
+		}
+
+		if at(input, pos) == '-' {
+			pos++
+			num, pos = strtoi(input, pos)
+			fmt.Printf("  sub rax, %d\n", num)
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "予期しない文字です: %c\n", at(input, pos))
 		return 1
 	}
-	fmt.Printf("  mov rax, %d\n", n)
+
 	fmt.Printf("  ret\n")
 
 	return 0
